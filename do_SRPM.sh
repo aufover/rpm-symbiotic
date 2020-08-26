17274109cf7c4aaed8313578570312e37e644dfd
Lukas Zaoral <lzaoral@redhat.com>
2020-08-26 10:18:31 +0200

    do_SRPM.sh: Fedora 33 rebuild

--- do_SRPM.sh
+++ do_SRPM.sh
@@ -58,7 +58,7 @@ tar -Jcf "symbiotic-$VER.tar.xz" "symbiotic-$VER"
 cat > symbiotic.spec << EOF
 Name:       $PKG
 Version:    $VER
-Release:    4%{?dist}
+Release:    5%{?dist}
 Summary:    Tool for analysis of sequential computer programs written in C
 License:    Free
 URL:        https://github.com/staticafi/%{name}
