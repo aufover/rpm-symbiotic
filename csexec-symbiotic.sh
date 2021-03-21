a453ffec987b6a267ebb2e4f9946f05c7b1e0d97
Lukas Zaoral <lzaoral@redhat.com>
2021-03-21 11:44:41 +0100

    csexec: Capture the output instead of printing it out

--- csexec-symbiotic.sh
+++ csexec-symbiotic.sh
@@ -5,7 +5,7 @@ usage() {
 USAGE: $0 -s SYMBIOTIC_ARGS ARGV
 1) Build the source with gllvm and CFLAGS internally used by Symbiotic and
    LDFLAGS='-Wl,--dynamic-linker=/usr/bin/csexec-loader'.
-2) CSEXEC_WRAP_CMD=$'--skip-ld-linux\acsexec-symbiotic\a-s\a--prp=memsafety' make check
+2) CSEXEC_WRAP_CMD=$'--skip-ld-linux\acsexec-symbiotic\a-l\aLOG_DIR\a-s\a--prp=memsafety' make check
 3) Wait for some time.
 4) ...
 5) Profit!
@@ -14,8 +14,11 @@ EOF
 
 [[ $# -eq 0 ]] && usage && exit 1
 
-while getopts "s:h" opt; do
+while getopts "l:s:h" opt; do
   case "$opt" in
+    l)
+      LOGDIR="$OPTARG"
+      ;;
     s)
       SYMBIOTIC=($OPTARG)
       ;;
@@ -31,9 +34,15 @@ done
 shift $((OPTIND - 1))
 ARGV=("$@")
 
+if [ -z "$LOGDIR" ]; then
+  echo "-l LOGDIR option is mandatoty!"
+  exit 1
+fi
+
 # Run!
-get-bc "${ARGV[0]}" 1> /dev/tty 2>&1
-symbiotic "${SYMBIOTIC[@]}" --argv="'${ARGV[*]}'" "${ARGV[0]}.bc" 1> /dev/tty 2>&1
+get-bc "${ARGV[0]}" > /dev/null || exit 1
+symbiotic "${SYMBIOTIC[@]}" --argv="'${ARGV[*]}'" "${ARGV[0]}.bc" \
+  2> "$LOGDIR/pid-$$.err" > "$LOGDIR/pid-$$.out"
 
 # Continue
 exec $(csexec --print-ld-exec-cmd) "${ARGV[@]}"
